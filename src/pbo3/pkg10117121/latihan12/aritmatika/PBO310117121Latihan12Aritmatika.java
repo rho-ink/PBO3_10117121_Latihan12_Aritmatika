8bcc932aa229332c068f7fcd8bb8e6dd9a411868
CHEEKIBREEKIIVDAMKE <CHEEKIBREEKIIVDAMKE@DESKTOP-S13CU08>
2018-09-23 18:58:23 +0700

    rho-ink

---
+++ src/pbo3/pkg10117121/latihan12/aritmatika/PBO310117121Latihan12Aritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117121.latihan12.aritmatika;
+
+/**
+ *
+ * @author CHEEKIBREEKIIVDAMKE
+ *NAMA                 : M Rifqi S M
+ *KELAS                : PBO-3 
+ *NIM                  : 10117121
+ *Program Deskripsi    : Program ini berisi program menampilkan proses aritmetik
+ */
+public class PBO310117121Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+
+    }
+    
+}
